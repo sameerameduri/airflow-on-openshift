8df40f1891d3c91578f60ae4c934ed1caf7331db
Bhavya sameera Meduri <MedusaB@corp.cableone.net>
2023-10-05 10:16:35 -0500

    adding bash dag examples

---
+++ dags/bash_dag_example.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    'owner': 'bmeduri',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=2)
+
+}
+
+with DAG(
+    dag_id='first_bash_dag',
+    default_args=default_args,
+    description='This is my first dag',
+    start_date=datetime(2023, 10, 5),
+    schedule_interval='@daily'
+) as dag:
+    task1 = BashOperator(
+        task_id='first_task',
+        bash_command="echo hello world, this is the first task!"
+    )
+    task1
--- dags/cron_dag_example.py
+++ dags/cron_dag_example.py
@@ -5,7 +5,7 @@ from airflow.operators.bash import BashOperator
 
 
 default_args = {
-    'owner': 'coder2j',
+    'owner': 'bmeduri',
     'retries': 5,
     'retry_delay': timedelta(minutes=5)
 }
