f6b8e709b88daa54bd9a3863689b82d93ffc5db2
Bhavya sameera Meduri <MedusaB@corp.cableone.net>
2023-10-05 10:33:09 -0500

    adding bash dag v2

--- dags/bash_dag_example.py
+++ dags/bash_dag_example.py
@@ -11,7 +11,7 @@ default_args = {
 }
 
 with DAG(
-    dag_id='first_bash_dag',
+    dag_id='first_bash_dag_v2',
     default_args=default_args,
     description='This is my first dag',
     start_date=datetime(2023, 10, 5),
@@ -21,4 +21,11 @@ with DAG(
         task_id='first_task',
         bash_command="echo hello world, this is the first task!"
     )
-    task1
+    
+
+    task2 = BashOperator(
+        task_id='second_task',
+        bash_command="echo hello world, this is the second task and will run after first"
+    )
+
+    task1.set_downstream(task2)
