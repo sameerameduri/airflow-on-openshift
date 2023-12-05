f621aa0202da039c1a5735aa218c1c7ad4510b0c
Bhavya sameera Meduri <MedusaB@corp.cableone.net>
2023-12-05 14:25:15 -0600

    script for cleaning logs

--- dags/bash_dag_example.py
+++ dags/bash_dag_example.py
@@ -2,6 +2,15 @@ from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+# import the logging module
+import logging
+
+# get the airflow.task logger
+task_logger = logging.getLogger("airflow.task")
+
+task_logger.debug("This log is at the level of DEBUG")
+        # each of these lines produces a log statement
+print("This log is created with a print statement")
 
 default_args = {
     'owner': 'bmeduri',
