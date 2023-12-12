472a104f4c17109a0d616847326505c8b017b74d
Bhavya sameera Meduri <MedusaB@corp.cableone.net>
2023-12-12 09:51:38 -0600

    chown

--- scripts/easy_install.sh
+++ scripts/easy_install.sh
@@ -48,10 +48,8 @@ helm_install(){
   CHART_GID=$(oc get project ${PROJECT} -o jsonpath="{['metadata.annotations.openshift\.io/sa\.scc\.supplemental-groups']}" | sed "s@/.*@@")
 
   echo "UID/GID: $CHART_UID/$CHART_GID"
-  FROM bitnami/postgresql:latest
-  USER 0
-  RUN chown CHART_UID:CHART_GID -r daemon /opt/bitnami
-  RUN chown CHART_UID:CHART_GID -r /bitnami/postgresql
+  chown CHART_UID:CHART_GID -r daemon /opt/bitnami
+  chown CHART_UID:CHART_GID -r /bitnami/postgresql
   
 
 
